6191a3217d4fb5b568e453e5db3cc152d1d16864
Saad Baradan <saadbaradan@gmail.com>
2019-10-22 10:58:44 -0600

    Create form for user login and new account

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,7 +1,12 @@
 <template>
   <Page>
     <ActionBar title="Booj's Book List" />
-    <StackLayout class="background"></StackLayout>
+    <StackLayout class="home">
+      <TextField class="username" hint=" UserName..." />
+      <TextField class="password" hint=" Password..." />
+      <Button class="log-in" text="Log In" />
+      <Button class="create-account" text="Create Account" />
+    </StackLayout>
   </Page>
 </template>
 
@@ -21,11 +26,41 @@ ActionBar {
   color: #ffffff;
 }
 
-.background {
+.home {
   background-image: url("~/assets/images/library.jpeg");
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
   height: 1000;
+}
+
+.username,
+.password {
+  background-color: white;
+  border: 2px solid black;
+  color: black;
+  height: 8%;
+  width: 70%;
+}
+
+.username {
+  margin: 20% auto 2% auto;
+}
+
+.password {
+  margin: 0 auto 2% auto;
+}
+
+.log-in {
+  background: #53ba82;
+  color: white;
+  font-size: 20px;
+  height: 8%;
+  width: 70%;
+}
+
+.create-account {
+  color: white;
+  margin-top: 5%;
 }
 </style>
