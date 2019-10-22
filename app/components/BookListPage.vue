331633d6d5e00b68c92a4b14eac42d40cd36142c
Saad Baradan <saadbaradan@gmail.com>
2019-10-22 15:06:36 -0600

    Style book cards and display author/title information

--- app/components/BookListPage.vue
+++ app/components/BookListPage.vue
@@ -4,8 +4,9 @@
     <StackLayout class="book-list-page">
       <ListView for="book in Books" @itemTap="goToBookDetails($event)">
         <v-template>
-          <StackLayout>
+          <StackLayout class="book-card">
             <Label :text="book.title" />
+            <Label class="by" :text="book.author" editable="false" />
           </StackLayout>
         </v-template>
       </ListView>
@@ -21,19 +22,19 @@ export default {
       Books: [
         {
           title: "Critique of Pure Reason",
-          author: "Immanuel Kant",
+          author: "By Immanuel Kant",
           publication: 1781,
           id: 0
         },
         {
           title: "Being & Time",
-          author: "Martin Heidegger",
+          author: "By Martin Heidegger",
           publication: 1927,
           id: 1
         },
         {
           title: "Being & Nothingness",
-          author: "Martin Heidegger",
+          author: "By Jean Paul Sartre",
           publication: 1943,
           id: 2
         }
@@ -62,5 +63,21 @@ ListView {
   background-position: center;
   background-size: cover;
   height: 1000;
+}
+
+.book-card {
+ background: #FAFAF5;
+ color: #53ba82;
+ font-size: 24;
+ font-weight: bold;
+ height: 80;
+ margin: 5;
+ padding-left: 25%;
+ padding-top: 5%;
+}
+
+.by {
+  font-size: 12;
+  height: 20;
 }
 </style>
