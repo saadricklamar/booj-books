d3beb0a48cd074dd1b8791b53c72bda806263611
Saad Baradan <saadbaradan@gmail.com>
2019-10-25 12:56:45 -0600

    Lint files and add home page screenshot

--- app/tests/App.js
+++ app/tests/App.js
@@ -1,22 +1,25 @@
 var App = require("../components/App.vue");
-const chai = require('chai');
+const chai = require("chai");
 const expect = chai.expect;
-const spies = require('chai-spies');
+const spies = require("chai-spies");
 chai.use(spies);
 
 describe("App", function() {
   it("should have an initial state", function() {
-    let data = {
-      books: [],
-      error: '',
-      title: '',
-      author: '',
-      publication: ''
-    }
-    expect(data).to.be.an('object')
+    let spy = chai.spy.on(App, {
+      data: {
+        books: [],
+        error: "",
+        title: "",
+        author: "",
+        publication: ""
+      }
+    });
+
+    expect(spy).to.have.been.called;
   });
   it("should call fetchBooks when mounted", function() {
-    let spy = chai.spy.on(App, 'fetchBooks');
+    let spy = chai.spy.on(App, "fetchBooks");
     expect(spy).to.have.been.called;
   });
 });
